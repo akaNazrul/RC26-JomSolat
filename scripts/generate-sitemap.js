d2e3e5f7d3262936f5f71b333714718c5463a8d4
Nazrul Hakim <hakimnazrul32@gmail.com>
2026-03-10 19:05:54 +0800

    add sitemap setup

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import fs from 'fs';
+const base = 'https://rc26-jomsolat.vercel.app';
+const pages = [
+  '/',
+  '/landing',
+  '/home',
+  '/prayer-times',
+  '/mosque-info',
+  '/facilities',
+  '/parking',
+  '/feed',
+  '/profile',
+  '/account-settings',
+  '/admin-dashboard',
+  '/login',
+  '/signup',
+  '/forgot-password',
+  '/reset-password'
+];
+
+const urls = pages.map(p => `${base}${p}`).map(url => `  <url>\n    <loc>${url}</loc>\n  </url>`).join('\n');
+const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls}\n</urlset>\n`;
+
+fs.mkdirSync('public', { recursive: true });
+fs.writeFileSync('public/sitemap.xml', xml, 'utf8');
+console.log('Wrote public/sitemap.xml');
